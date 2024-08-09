ae9a098f5efa1d6a02fd5fac39d639ed2cb48ece
cameronrulten <cbrulten@umn.edu>
2024-08-09 14:41:57 +0100

    Update entrypoint to allow either entry to bash or jupyter notebook

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,24 @@
-#!/bin/bash --login
-set -e #o pipefail
-#conda init bash
-#source /root/.bashrc
-#conda activate fermipy-v1-0-1
-export PATH=${PATH}:/miniconda3/envs/fermipy-v1-2-2/bin
-python /temp/start-env.py
-#jupyter-lab --notebook-dir=/workdir --ip='0.0.0.0' --port=8888 --no-browser --allow-root
+#!/bin/bash
+set -e
+
+# Activate the conda environment
+source activate $condaEnvName
+
+# If no arguments are passed, default to starting a Jupyter notebook
+if [ $# -eq 0 ]; then
+    exec jupyter notebook --notebook-dir=/workdir --ip='0.0.0.0' --port=8888 --no-browser --allow-root
+else
+    # Otherwise, run the command provided (like /bin/bash)
+    exec "$@"
+fi
+
+
+
+# #!/bin/bash --login
+# set -e #o pipefail
+# #conda init bash
+# #source /root/.bashrc
+# #conda activate fermipy-v1-0-1
+# export PATH=${PATH}:/miniconda3/envs/fermipy-v1-2-2/bin
+# python /temp/start-env.py
+# #jupyter-lab --notebook-dir=/workdir --ip='0.0.0.0' --port=8888 --no-browser --allow-root
