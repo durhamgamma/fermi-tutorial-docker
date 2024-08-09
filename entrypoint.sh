f1e232a5bc5a439e801e56ce42f5d08795779fd5
cameronrulten <cbrulten@umn.edu>
2024-08-09 14:25:13 +0100

    Update path to fermipy in entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ set -e #o pipefail
 #conda init bash
 #source /root/.bashrc
 #conda activate fermipy-v1-0-1
-export PATH=${PATH}:/miniconda3/envs/fermipy-v1-0-1/bin
+export PATH=${PATH}:/miniconda3/envs/fermipy-v1-2-2/bin
 python /temp/start-env.py
 #jupyter-lab --notebook-dir=/workdir --ip='0.0.0.0' --port=8888 --no-browser --allow-root
