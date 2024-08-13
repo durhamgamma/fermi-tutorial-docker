27d181b7cd9eed22f4d87d68c9815cd7a23056e8
cameronrulten <cbrulten@umn.edu>
2024-08-13 16:38:39 +0100

    Update entrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 set -e
 
+# Source the conda.sh script to ensure conda is initialized
+source /miniconda3/etc/profile.d/conda.sh
+
 # Check if the container is being launched into a Bash shell
 if [ "$1" == "/bin/bash" ]; then
     # No need to re-source environment variables; just activate Conda
-    source ~/.bashrc
+    conda activate fermipy-v1-2-2
     exec "$@"
 else
     # Otherwise, assume it's being launched for Jupyter and source env variables
-    source ~/.bashrc
+    #source ~/.bashrc
     conda activate fermipy-v1-2-2
     source /temp/env_vars.sh
 
