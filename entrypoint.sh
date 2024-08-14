fbae32b63d7e3c9601aee57ffbda698ced17f874
cameronrulten <cbrulten@umn.edu>
2024-08-14 11:01:00 +0100

    Tidy up the entrypoint.sh script. Delete commented out old code

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,6 @@ else
     # Otherwise, assume it's being launched for Jupyter and source env variables
     source ~/.bashrc
     conda activate fermipy-v1-2-2
-    #source /temp/env_vars.sh
 
     # If no arguments are passed, default to starting a Jupyter notebook
     if [ $# -eq 0 ]; then
@@ -24,37 +23,4 @@ else
     fi
 fi
 
-
-
-
-
-# #!/bin/bash
-# set -e
-
-# # Source the bashrc to ensure all environment variables are loaded
-# source ~/.bashrc
-
-# # Set the PFILES environment variable if not already set
-# #export PFILES=".:/root/pfiles;/miniconda3/envs/fermipy-v1-2-2/share/fermitools/syspfiles"
-
-# # Activate the conda environment
-# #source activate $condaEnvName
-# source activate fermipy-v1-2-2
-
-# # If no arguments are passed, default to starting a Jupyter notebook
-# if [ $# -eq 0 ]; then
-#     exec jupyter notebook --notebook-dir=/workdir --ip='0.0.0.0' --port=8888 --no-browser --allow-root
-# else
-#     # Otherwise, run the command provided (like /bin/bash)
-#     exec "$@"
-# fi
-
-
-# #!/bin/bash --login
-# set -e #o pipefail
-# #conda init bash
-# #source /root/.bashrc
-# #conda activate fermipy-v1-0-1
-# export PATH=${PATH}:/miniconda3/envs/fermipy-v1-2-2/bin
-# python /temp/start-env.py
-# #jupyter-lab --notebook-dir=/workdir --ip='0.0.0.0' --port=8888 --no-browser --allow-root
+#end of entrypoint.sh file
